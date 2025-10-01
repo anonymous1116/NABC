8948073f0a76cb4c7899093eb743f60b3e817cc1
anonymous1116 <hhyun116@gmail.com>
2025-10-01 11:40:20 -0400

    change name

--- NPE/NPE_training.py
+++ NPE/NPE_training.py
@@ -2,7 +2,7 @@ import sys
 import torch
 import numpy as np
 from sbi import utils
-from sbi.inference import SNPE
+from sbi.inference import SNPE,SNLE
 import pickle
 import os
 import argparse
@@ -26,7 +26,13 @@ def main(args):
     X = simulators(theta)
 
     # Create inference object
-    inference = SNPE(prior=priors, density_estimator=args.cond_den)
+    if args.method == "NPE":
+        inference = SNPE(prior=priors, density_estimator=args.cond_den)
+    elif args.method == "NLE":
+        inference = SNLE(prior=priors, density_estimator=args.cond_den)
+    else:
+        raise ValueError(f"Unsupported method: {args.method}. Choose 'NPE' or 'NLE'.")
+        
     inference = inference.append_simulations(theta, X)
 
     # Train the density estimator and build the posterior
@@ -39,7 +45,7 @@ def main(args):
     print(f"Training with {args.cond_den}")
 
     # Define the output directory
-    output_dir = f"./NPE_nets/{args.task}/J_{int(args.num_training/1000)}K"
+    output_dir = f"./{args.method}_nets/{args.task}/J_{int(args.num_training/1000)}K"
 
     # Create the directory if it doesn't exist
     if not os.path.exists(output_dir):
@@ -61,6 +67,7 @@ def get_args():
     parser = argparse.ArgumentParser(description="Run simulations and inference.")
     parser.add_argument('--task', type=str, default='twomoons', help='Simulation type: twomoons, MoG, Lapl, GL_U or SLCP')
     parser.add_argument('--seed', type=int, default=1, help='Random seed for reproducibility')
+    parser.add_argument('--method', type=str, default='NPE', help='NPE or NLE')
     parser.add_argument('--cond_den', type=str, default='maf', help='Conditional density estimator type: mdn, maf, nsf')
     parser.add_argument('--num_training', type=int, default=500_000, help='Number of simulations to run')
     return parser.parse_args()
